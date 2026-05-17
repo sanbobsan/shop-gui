0892a0006468721cf3d99995307f265dc9a7aa10
sanbob <saintsanbob@gmail.com>
2026-05-17 10:46:18 +0800

    feat: implement tabbed interface in main application

--- src/app/main.py
+++ src/app/main.py
@@ -2,7 +2,30 @@ import flet as ft
 
 
 def main(page: ft.Page) -> None:
-    page.add(ft.Text("shop-gui", size=50))
+
+    tabs = ft.Tabs(
+        expand=True,
+        length=2,
+        content=ft.Column(
+            [
+                ft.TabBar(
+                    tabs=[
+                        ft.Tab("Tab 1"),
+                        ft.Tab("Tab 2"),
+                    ],
+                ),
+                ft.TabBarView(
+                    expand=True,
+                    controls=[
+                        ft.Container(content=ft.Text("Tab 1 content")),
+                        ft.Container(content=ft.Text("Tab 2 content")),
+                    ],
+                ),
+            ],
+        ),
+    )
+
+    page.add(ft.SafeArea(expand=True, content=tabs))
 
 
 ft.run(main)
