adcb01b543dab3363f9c9d110749408b3fbec1c0
sanbob <saintsanbob@gmail.com>
2026-05-17 17:10:19 +0800

    feat: implement CategoryContainer for category tab

--- src/app/view/categories.py
+++ src/app/view/categories.py
@@ -19,3 +19,28 @@ class CategoryCard(ft.Card):
             self.name: str = category_obj.name
 
         self.content = ft.Text(self.name)
+
+
+class CategoryContainer(ft.Container):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.button = ft.Button("Add Category", on_click=self.add_category)
+        self.text_field = ft.TextField(label="Category Name")
+        self.cards = ft.Column()
+
+        self.content = ft.Column(
+            [
+                self.button,
+                self.text_field,
+                self.cards,
+            ]
+        )
+
+    def add_category(self) -> None:
+        category_name: str = self.text_field.value
+        if not category_name:
+            return
+        category_card = CategoryCard(category_name)
+        self.cards.controls.append(category_card)
+        self.update()
