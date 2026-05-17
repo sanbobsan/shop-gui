474ea055b0dfd754da2ca46deae48377bcf9e2af
sanbob <saintsanbob@gmail.com>
2026-05-17 16:57:12 +0800

    feat: implement CategoryService for category management

---
+++ src/app/service/category.py
@@ -0,0 +1,19 @@
+from app.database import Category
+from app.repository import CategoryRepository
+
+
+class CategoryService:
+    def __init__(self, repository: CategoryRepository) -> None:
+        self.repo: CategoryRepository = repository
+
+    def create_category(self, name: str) -> Category:
+        return self.repo.create(name=name)
+
+    def get_category(self, id: int) -> Category | None:
+        return self.repo.get(id)
+
+    def delete_category(self, id: int) -> bool:
+        return self.repo.delete(id)
+
+    def get_all_categories(self) -> list[Category]:
+        return self.repo.get_all()
