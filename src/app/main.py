9f2b1236df08cbb84d85d5e536a8cbf3cd1c7960
sanbob <saintsanbob@gmail.com>
2026-05-18 00:28:51 +0800

    feat: initialize database tables in main function

--- src/app/main.py
+++ src/app/main.py
@@ -1,11 +1,13 @@
 import flet as ft
 
+from app.database import create_tables
 from app.views import CategoryContainer, ProductContainer
 
 
 def main(page: ft.Page) -> None:
-
     page.title = "shop-gui"
+    create_tables()
+
     tabs = ft.Tabs(
         expand=True,
         length=2,
